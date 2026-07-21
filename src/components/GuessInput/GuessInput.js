80a00e540c6cbf5d1b9e4c58c75f622a409cae58
Lukashina Natalia <natulya.lukashina@bk.ru>
2026-07-21 22:41:41 +0300

    create component with word input form

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+function GuessInput(answer) {
+  const [guess, setGuess] = React.useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (guess.length !== 5) {
+      e.target.classList.add("invalid");
+
+      return;
+    }
+
+    console.log(answer);
+    console.log({ guess });
+
+    setGuess("");
+  };
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        minLength={5}
+        maxLength={5}
+        pattern="[a-zA-Z]{5}"
+        required
+        value={guess}
+        onChange={(e) => {
+          const nextGuess = e.target.value.toUpperCase();
+          setGuess(nextGuess);
+        }}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
